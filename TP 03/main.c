f96d785de686be9afca3bb1d7ce64af3281d6ff6
Luiz Lima <95664410+Leokto@users.noreply.github.com>
2023-07-06 22:25:06 -0300

    First commit.

---
+++ TP 03/main.c
@@ -0,0 +1,18 @@
+#include "sorting.h"
+
+int main(){
+    int N = 10000, vector[N];
+    srand(time(0));
+    for(int i = 0; i < 10000; i++){
+        vector[i] = rand();
+    }
+    bubbleSort(vector, N);
+    printf("Sorted array: \n");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", vector[i]);
+        if( i % 9 == 0){
+            printf("\n");
+        }
+    }
+    return 0;
+}
---
+++ TP 03/sorting.c
@@ -0,0 +1,15 @@
+#include "sorting.h"
+
+void bubbleSort(int array[], int size) {
+    int i, j;
+    for (i = 0; i < size - 1; i++) {
+        for (j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                // Swap elements
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
