698010cb3958815481ffdd3378090d75cfaf7ddc
Leokto <louiscaesar40s@gmail.com>
2023-07-08 13:29:01 -0300

    Binary sort tree made.

--- TP 03/main.c
+++ TP 03/main.c
@@ -1,21 +1,18 @@
 #include "sorting.h"
 
 int main(){
-    int N = 10000, vector[N];
+    int N = 10000, vector[N], counter = 0;
+    struct TreeNode* root = NULL;
+    root = insertNode(root, rand());
     srand(time(0));
-    for(int i = 0; i < 10000; i++){
-        vector[i] = rand();
+    for(int i = 0; i < 10000 - 1; i++){
+        insertNode(root, rand());
     }
     ///bubbleSort(vector, N);
     ///radixSort(vector, N);
-    quickSort(vector, 0, N - 1);
+    //quickSort(vector, 0, N - 1);
     printf("Sorted array: \n");
-    for (int i = 0; i < N; i++) {
-        printf("%d ", vector[i]);
-        if( i % 9 == 0){
-            printf("\n");
-        }
-    }
+    inorderTraversal(root, &counter);
     return 0;
 }
 
--- TP 03/sorting.c
+++ TP 03/sorting.c
@@ -43,3 +43,38 @@ void quickSort(int vector[], int low, int high) {
         quickSort(vector, pi + 1, high);
     }
 }
+
+// Binary Tree Sort
+struct TreeNode* createNode(int value) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct TreeNode* insertNode(struct TreeNode* root, int value) {
+    if (root == NULL) {
+        return createNode(value);
+    }
+
+    if (value < root->data) {
+        root->left = insertNode(root->left, value);
+    } else if (value > root->data) {
+        root->right = insertNode(root->right, value);
+    }
+
+    return root;
+}
+
+void inorderTraversal(struct TreeNode* root, int *ptr) {
+    if (root != NULL) {
+        inorderTraversal(root->left, ptr);
+        printf("%d ", root->data);
+        *ptr += 1;
+        if(*ptr % 9 == 0){
+            printf("\n");
+        }
+        inorderTraversal(root->right, ptr);
+    }
+}
